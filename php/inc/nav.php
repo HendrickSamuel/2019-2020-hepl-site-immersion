6421d4a6083e4337bd060d55ef739da95c1d845f
Solitan <53486526+Solitan@users.noreply.github.com>
2020-04-09 16:22:51 +0200

    Base en php

---
+++ php/inc/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar py-2 navbar-light navbar-expand-md">
+    <!-- logo -->
+    <a href="#" class="navbar-brand text-uppercase text-vert font-italic">
+        <img src="./img/Logo_HEPL.png" height="28" alt="LogoHEPL">
+    </a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarLinks">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarLinks">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link hvr-underline-from-center">inscription</a>
+            </li>
+            <li class="nav-item pr-3">
+                <a href="#" class="nav-link hvr-underline-from-center">cours</a>
+            </li>
+        </ul>
+    </div>
+</nav>
