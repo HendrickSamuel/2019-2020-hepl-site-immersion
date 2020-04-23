6485cc1780fbb79debe6901ed60ca6ce8fe57c40
Solitan <53486526+Solitan@users.noreply.github.com>
2020-04-23 18:36:56 +0200

    Style.scss v0.2+ jquerymin + bootstrap + recent

---
+++ creationHoraire.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php
+        require(__DIR__."/php/inc/head.php");
+        require_once (__DIR__."/php/includes.php");
+    ?>
+    <title>Horaire</title>
+</head>
+
+<body>
+    <?php require(__DIR__."/php/inc/nav.php"); ?>
+
+    <section id="jour-plage-horaire">
+    </section>
+    
+    <section class="py-5 plages-horaire">
+        <div class="container">
+            <h2>Plage 1</h2>
+            <div class="row" id="plage-1">
+                <div class="col-md-4">
+                    <div class="row zone-cours-choisi">
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <!-- <span class="badge badge-pill badge-primary ">7/10</span> -->
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="carte-cours-vide"></div> -->
+                </div>
+                <div class="col-md-8">
+                    <div class="row zone-cours-dispo">
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <span class="badge badge-pill badge-primary ">7/10</span>
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <span class="badge badge-pill badge-primary ">7/10</span>
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reprehenderit non dolor voluptatum pariatur, hic molestiae corporis! Ex, alias tempore itaque impedit reprehenderit odio soluta nam deleniti quis exercitationem qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="py-5 plages-horaire">
+        <div class="container">
+            <h2>Plage 2</h2>
+            <div class="row" id="plage-2">
+                <div class="col-md-4">
+                    <div class="row zone-cours-choisi">
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <!-- <span class="badge badge-pill badge-primary ">7/10</span> -->
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="carte-cours-vide"></div> -->
+                </div>
+                <div class="col-md-8">
+                    <div class="row zone-cours-dispo">
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <span class="badge badge-pill badge-primary ">7/10</span>
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="carte-cours">
+                                <div class="nb-place-dispo">
+                                    <span class="badge badge-pill badge-primary ">7/10</span>
+                                </div>
+                                <div class="info-cours">
+                                    <div class="row px-2">
+                                        <div class="col-12 titre">
+                                            <h4>C++</h4>
+                                        </div>
+                                        <div class="col-12 description">
+                                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reprehenderit non dolor voluptatum pariatur, hic molestiae corporis! Ex, alias tempore itaque impedit reprehenderit odio soluta nam deleniti quis exercitationem qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum impedit atque consectetur nam architecto, assumenda delectus numquam nobis nostrum nemo aspernatur quod, asperiores quis sapiente mollitia distinctio blanditiis. Harum, sit.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
